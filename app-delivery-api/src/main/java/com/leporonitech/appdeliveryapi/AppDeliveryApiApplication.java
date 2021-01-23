ec28a05e721a0fb331c9a9b6d9c57e1850fdf072
Leporoni <leporoni@hotmail.com>
2021-01-23 12:35:37 -0300

    Added remote repository for app-delivery-api.

---
+++ app-delivery-api/src/main/java/com/leporonitech/appdeliveryapi/AppDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.leporonitech.appdeliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDeliveryApiApplication.class, args);
+	}
+
+}
---
+++ app-delivery-api/src/test/java/com/leporonitech/appdeliveryapi/AppDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leporonitech.appdeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
